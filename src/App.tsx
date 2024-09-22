9c54a961d2a74f9e4aec5ccf32a483bc2f361c18
TrueGulIlla <noinskijilla@gmail.com>
2024-09-22 14:19:25 +0300

    add lorem

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-
 const App = () => {
     return (
         <div>
-            
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab beatae consectetur corporis cupiditate dolor
+            harum, incidunt nobis placeat ratione repellendus similique tempore velit voluptas? Aperiam est nisi qui
+            reiciendis tenetur?
         </div>
     );
 };
